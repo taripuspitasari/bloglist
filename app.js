6ff03f52d1703b15e93f8705468dcad9bfe62a33
Tari Puspita Sari <taripuspitasari12@gmail.com>
2024-01-13 15:08:33 +0700

    initiate bloglist app

---
+++ app.js
@@ -0,0 +1,22 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(config.mongoUrl)
+  .then(() => {
+    logger.info("connected to mongoDB");
+  })
+  .catch(error => logger.error(error));
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
