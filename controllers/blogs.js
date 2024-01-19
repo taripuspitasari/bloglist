ffde4cd7ee0de2a2c11a91b8e7c5e5b41f814e86
Tari Puspita Sari <taripuspitasari12@gmail.com>
2024-01-19 14:02:10 +0700

    add token authentication

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,23 +1,40 @@
+const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user");
-  response.status(201).json(blogs);
+  response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response) => {
-  const {title, author, url, likes} = request.body;
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id).populate("user");
+  response.json(blog);
+});
 
-  const user = await User.findById(request.body.userId);
+const tokenExtractor = request => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
+
+blogsRouter.post("/", async (request, response) => {
+  const body = request.body;
+  const decodeToken = jwt.verify(tokenExtractor(request), process.env.SECRET);
+  if (!decodeToken.id) {
+    return response.status(401).json({error: "token invalid"});
+  }
+  const user = await User.findById(decodeToken.id);
 
   const blog = new Blog({
-    title,
-    author,
-    url,
-    likes,
-    user: user.id,
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id,
   });
 
   const savedBlog = await blog.save();
